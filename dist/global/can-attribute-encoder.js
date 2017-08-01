1a99ab87e744ee6fbe46439ebb390ecdc494a1d0
Kevin Phillips <kevin@bitovi.com>
2017-08-01 08:59:06 -0500

    0.2.0

---
+++ dist/global/can-attribute-encoder.js
@@ -0,0 +1,214 @@
+/*[global-shim-start]*/
+(function(exports, global, doEval){ // jshint ignore:line
+	var origDefine = global.define;
+
+	var get = function(name){
+		var parts = name.split("."),
+			cur = global,
+			i;
+		for(i = 0 ; i < parts.length; i++){
+			if(!cur) {
+				break;
+			}
+			cur = cur[parts[i]];
+		}
+		return cur;
+	};
+	var set = function(name, val){
+		var parts = name.split("."),
+			cur = global,
+			i, part, next;
+		for(i = 0; i < parts.length - 1; i++) {
+			part = parts[i];
+			next = cur[part];
+			if(!next) {
+				next = cur[part] = {};
+			}
+			cur = next;
+		}
+		part = parts[parts.length - 1];
+		cur[part] = val;
+	};
+	var useDefault = function(mod){
+		if(!mod || !mod.__esModule) return false;
+		var esProps = { __esModule: true, "default": true };
+		for(var p in mod) {
+			if(!esProps[p]) return false;
+		}
+		return true;
+	};
+
+	var hasCjsDependencies = function(deps) {
+		return (
+			deps[0] === "require" &&
+			deps[1] === "exports" &&
+			deps[2] === "module"
+		);
+	};
+
+	var modules = (global.define && global.define.modules) ||
+		(global._define && global._define.modules) || {};
+	var ourDefine = global.define = function(moduleName, deps, callback){
+		var module;
+		if(typeof deps === "function") {
+			callback = deps;
+			deps = [];
+		}
+		var args = [],
+			i;
+		for(i =0; i < deps.length; i++) {
+			args.push( exports[deps[i]] ? get(exports[deps[i]]) : ( modules[deps[i]] || get(deps[i]) )  );
+		}
+		// CJS has no dependencies but 3 callback arguments
+		if (hasCjsDependencies(deps) || (!deps.length && callback.length)) {
+			module = { exports: {} };
+			args[0] = function(name) {
+				return exports[name] ? get(exports[name]) : modules[name];
+			};
+			args[1] = module.exports;
+			args[2] = module;
+		}
+		// Babel uses the exports and module object.
+		else if(!args[0] && deps[0] === "exports") {
+			module = { exports: {} };
+			args[0] = module.exports;
+			if(deps[1] === "module") {
+				args[1] = module;
+			}
+		} else if(!args[0] && deps[0] === "module") {
+			args[0] = { id: moduleName };
+		}
+
+		global.define = origDefine;
+		var result = callback ? callback.apply(null, args) : undefined;
+		global.define = ourDefine;
+
+		// Favor CJS module.exports over the return value
+		result = module && module.exports ? module.exports : result;
+		modules[moduleName] = result;
+
+		// Set global exports
+		var globalExport = exports[moduleName];
+		if(globalExport && !get(globalExport)) {
+			if(useDefault(result)) {
+				result = result["default"];
+			}
+			set(globalExport, result);
+		}
+	};
+	global.define.orig = origDefine;
+	global.define.modules = modules;
+	global.define.amd = true;
+	ourDefine("@loader", [], function(){
+		// shim for @@global-helpers
+		var noop = function(){};
+		return {
+			get: function(){
+				return { prepareGlobal: noop, retrieveGlobal: noop };
+			},
+			global: global,
+			__exec: function(__load){
+				doEval(__load.source, global);
+			}
+		};
+	});
+}
+)({},window,function(__$source__, __$global__) { // jshint ignore:line
+	eval("(function() { " + __$source__ + " \n }).call(__$global__);");
+}
+)
+/*can-attribute-encoder@0.1.0#can-attribute-encoder*/
+define('can-attribute-encoder', [
+    'require',
+    'exports',
+    'module',
+    'can-namespace',
+    'can-util/js/dev/dev'
+], function (require, exports, module) {
+    var namespace = require('can-namespace');
+    var dev = require('can-util/js/dev/dev');
+    function each(items, callback) {
+        for (var i = 0; i < items.length; i++) {
+            callback(items[i], i);
+        }
+    }
+    function makeMap(str) {
+        var obj = {}, items = str.split(',');
+        each(items, function (name) {
+            obj[name] = true;
+        });
+        return obj;
+    }
+    var caseMattersAttributes = makeMap('allowReorder,attributeName,attributeType,autoReverse,baseFrequency,baseProfile,calcMode,clipPathUnits,contentScriptType,contentStyleType,diffuseConstant,edgeMode,externalResourcesRequired,filterRes,filterUnits,glyphRef,gradientTransform,gradientUnits,kernelMatrix,kernelUnitLength,keyPoints,keySplines,keyTimes,lengthAdjust,limitingConeAngle,markerHeight,markerUnits,markerWidth,maskContentUnits,maskUnits,patternContentUnits,patternTransform,patternUnits,pointsAtX,pointsAtY,pointsAtZ,preserveAlpha,preserveAspectRatio,primitiveUnits,repeatCount,repeatDur,requiredExtensions,requiredFeatures,specularConstant,specularExponent,spreadMethod,startOffset,stdDeviation,stitchTiles,surfaceScale,systemLanguage,tableValues,textLength,viewBox,viewTarget,xChannelSelector,yChannelSelector');
+    function camelCaseToSpinalCase(match, lowerCaseChar, upperCaseChar) {
+        return lowerCaseChar + '-' + upperCaseChar.toLowerCase();
+    }
+    function startsWith(allOfIt, startsWith) {
+        return allOfIt.indexOf(startsWith) === 0;
+    }
+    function endsWith(allOfIt, endsWith) {
+        return allOfIt.length - allOfIt.indexOf(endsWith) === endsWith.length;
+    }
+    var regexes = {
+        leftParens: /\(/g,
+        rightParens: /\)/g,
+        leftBrace: /\{/g,
+        rightBrace: /\}/g,
+        camelCase: /([a-z])([A-Z])/g,
+        forwardSlash: /\//g,
+        space: /\s/g,
+        uppercase: /[A-Z]/g,
+        uppercaseDelimiterThenChar: /:u:([a-z])/g,
+        caret: /\^/g,
+        dollar: /\$/g
+    };
+    var delimiters = {
+        prependUppercase: ':u:',
+        replaceSpace: ':s:',
+        replaceForwardSlash: ':f:',
+        replaceLeftParens: ':lp:',
+        replaceRightParens: ':rp:',
+        replaceLeftBrace: ':lb:',
+        replaceRightBrace: ':rb:',
+        replaceCaret: ':c:',
+        replaceDollar: ':d:'
+    };
+    var encoder = {};
+    encoder.encode = function (name) {
+        var encoded = name;
+        if (!caseMattersAttributes[encoded] && encoded.match(regexes.camelCase)) {
+            if (startsWith(encoded, 'on:') || endsWith(encoded, ':to') || endsWith(encoded, ':from') || endsWith(encoded, ':bind')) {
+                encoded = encoded.replace(regexes.uppercase, function (char) {
+                    return delimiters.prependUppercase + char.toLowerCase();
+                });
+            } else {
+                encoded = encoded.replace(regexes.camelCase, camelCaseToSpinalCase);
+            }
+        }
+        encoded = encoded.replace(regexes.space, delimiters.replaceSpace).replace(regexes.forwardSlash, delimiters.replaceForwardSlash).replace(regexes.leftParens, delimiters.replaceLeftParens).replace(regexes.rightParens, delimiters.replaceRightParens).replace(regexes.leftBrace, delimiters.replaceLeftBrace).replace(regexes.rightBrace, delimiters.replaceRightBrace).replace(regexes.caret, delimiters.replaceCaret).replace(regexes.dollar, delimiters.replaceDollar);
+        return encoded;
+    };
+    encoder.decode = function (name) {
+        var decoded = name;
+        decoded = decoded.replace(delimiters.replaceLeftParens, '(').replace(delimiters.replaceRightParens, ')').replace(delimiters.replaceLeftBrace, '{').replace(delimiters.replaceRightBrace, '}').replace(delimiters.replaceForwardSlash, '/').replace(delimiters.replaceSpace, ' ').replace(delimiters.replaceCaret, '^').replace(delimiters.replaceDollar, '$');
+        if (!caseMattersAttributes[decoded] && decoded.match(regexes.uppercaseDelimiterThenChar)) {
+            if (startsWith(decoded, 'on:') || endsWith(decoded, ':to') || endsWith(decoded, ':from') || endsWith(decoded, ':bind')) {
+                decoded = decoded.replace(regexes.uppercaseDelimiterThenChar, function (match, char) {
+                    return char.toUpperCase();
+                });
+            }
+        }
+        return decoded;
+    };
+    if (namespace.encoder) {
+        throw new Error('You can\'t have two versions of can-attribute-encoder, check your dependencies');
+    } else {
+        module.exports = namespace.encoder = encoder;
+    }
+});
+/*[global-shim-end]*/
+(function(){ // jshint ignore:line
+	window._define = window.define;
+	window.define = window.define.orig;
+}
+)();
