7e4405e26050081c85f984bf0be42492959189bc
Justin Meyer <justinbmeyer@gmail.com>
2018-01-26 14:27:53 -0600

    0.3.4

---
+++ dist/cjs/can-attribute-encoder.js
@@ -0,0 +1,83 @@
+/*can-attribute-encoder@0.3.3#can-attribute-encoder*/
+var namespace = require('can-namespace');
+var dev = require('can-log/dev/dev');
+function each(items, callback) {
+    for (var i = 0; i < items.length; i++) {
+        callback(items[i], i);
+    }
+}
+function makeMap(str) {
+    var obj = {}, items = str.split(',');
+    each(items, function (name) {
+        obj[name] = true;
+    });
+    return obj;
+}
+var caseMattersAttributes = makeMap('allowReorder,attributeName,attributeType,autoReverse,baseFrequency,baseProfile,calcMode,clipPathUnits,contentScriptType,contentStyleType,diffuseConstant,edgeMode,externalResourcesRequired,filterRes,filterUnits,glyphRef,gradientTransform,gradientUnits,kernelMatrix,kernelUnitLength,keyPoints,keySplines,keyTimes,lengthAdjust,limitingConeAngle,markerHeight,markerUnits,markerWidth,maskContentUnits,maskUnits,patternContentUnits,patternTransform,patternUnits,pointsAtX,pointsAtY,pointsAtZ,preserveAlpha,preserveAspectRatio,primitiveUnits,repeatCount,repeatDur,requiredExtensions,requiredFeatures,specularConstant,specularExponent,spreadMethod,startOffset,stdDeviation,stitchTiles,surfaceScale,systemLanguage,tableValues,textLength,viewBox,viewTarget,xChannelSelector,yChannelSelector');
+function camelCaseToSpinalCase(match, lowerCaseChar, upperCaseChar) {
+    return lowerCaseChar + '-' + upperCaseChar.toLowerCase();
+}
+function startsWith(allOfIt, startsWith) {
+    return allOfIt.indexOf(startsWith) === 0;
+}
+function endsWith(allOfIt, endsWith) {
+    return allOfIt.length - allOfIt.indexOf(endsWith) === endsWith.length;
+}
+var regexes = {
+    leftParens: /\(/g,
+    rightParens: /\)/g,
+    leftBrace: /\{/g,
+    rightBrace: /\}/g,
+    camelCase: /([a-z])([A-Z])/g,
+    forwardSlash: /\//g,
+    space: /\s/g,
+    uppercase: /[A-Z]/g,
+    uppercaseDelimiterThenChar: /:u:([a-z])/g,
+    caret: /\^/g,
+    dollar: /\$/g,
+    at: /@/g
+};
+var delimiters = {
+    prependUppercase: ':u:',
+    replaceSpace: ':s:',
+    replaceForwardSlash: ':f:',
+    replaceLeftParens: ':lp:',
+    replaceRightParens: ':rp:',
+    replaceLeftBrace: ':lb:',
+    replaceRightBrace: ':rb:',
+    replaceCaret: ':c:',
+    replaceDollar: ':d:',
+    replaceAt: ':at:'
+};
+var encoder = {};
+encoder.encode = function (name) {
+    var encoded = name;
+    if (!caseMattersAttributes[encoded] && encoded.match(regexes.camelCase)) {
+        if (startsWith(encoded, 'on:') || endsWith(encoded, ':to') || endsWith(encoded, ':from') || endsWith(encoded, ':bind')) {
+            encoded = encoded.replace(regexes.uppercase, function (char) {
+                return delimiters.prependUppercase + char.toLowerCase();
+            });
+        } else {
+            encoded = encoded.replace(regexes.camelCase, camelCaseToSpinalCase);
+        }
+    }
+    encoded = encoded.replace(regexes.space, delimiters.replaceSpace).replace(regexes.forwardSlash, delimiters.replaceForwardSlash).replace(regexes.leftParens, delimiters.replaceLeftParens).replace(regexes.rightParens, delimiters.replaceRightParens).replace(regexes.leftBrace, delimiters.replaceLeftBrace).replace(regexes.rightBrace, delimiters.replaceRightBrace).replace(regexes.caret, delimiters.replaceCaret).replace(regexes.dollar, delimiters.replaceDollar).replace(regexes.at, delimiters.replaceAt);
+    return encoded;
+};
+encoder.decode = function (name) {
+    var decoded = name;
+    if (!caseMattersAttributes[decoded] && decoded.match(regexes.uppercaseDelimiterThenChar)) {
+        if (startsWith(decoded, 'on:') || endsWith(decoded, ':to') || endsWith(decoded, ':from') || endsWith(decoded, ':bind')) {
+            decoded = decoded.replace(regexes.uppercaseDelimiterThenChar, function (match, char) {
+                return char.toUpperCase();
+            });
+        }
+    }
+    decoded = decoded.replace(delimiters.replaceLeftParens, '(').replace(delimiters.replaceRightParens, ')').replace(delimiters.replaceLeftBrace, '{').replace(delimiters.replaceRightBrace, '}').replace(delimiters.replaceForwardSlash, '/').replace(delimiters.replaceSpace, ' ').replace(delimiters.replaceCaret, '^').replace(delimiters.replaceDollar, '$').replace(delimiters.replaceAt, '@');
+    return decoded;
+};
+if (namespace.encoder) {
+    throw new Error('You can\'t have two versions of can-attribute-encoder, check your dependencies');
+} else {
+    module.exports = namespace.encoder = encoder;
+}
